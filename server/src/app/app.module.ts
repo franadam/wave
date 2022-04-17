76aeda2616e1e8c8abf63af9a1187ff13b730796
francois <francoisadam@hotmail.be>
2022-04-17 13:46:21 +0200

    feat(database): connect to postgres database

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 import Helmet from 'helmet';
 import * as session from 'express-session';
 import * as csurf from 'csurf';
+import { ValidationPipe } from '@nestjs/common';
 const xss = require('xss-clean');
 
 const PORT = process.env.PORT || 5000;
@@ -13,11 +14,12 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
   });
+  app.useGlobalPipes(new ValidationPipe());
   // app.use(csurf());
   app.use(xss());
   app.use(
     session({
-      secret: ' process.env.SESSION_KEY',
+      secret: 'process.env.SESSION_KEY',
       resave: false,
       saveUninitialized: false,
     }),
